7dea791450c73baf20f67cc226cf27a2ed5e1289
Siqi Liu <siqiliu@siqis-mbp.usca.ibm.com>
2024-05-29 17:27:15 -0700

    practice rotation

---
+++ rotate_matrix/RotateMatrix.Tests/RotateMatrix_Test.cs
@@ -0,0 +1,78 @@
+namespace RotateMatrix.Tests;
+using MainNS;
+
+public class JumpGameTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var obj = new RotateMatrix();
+        var res = obj.CanJump(new int[] { 2, 3, 1, 1, 4 });
+        Assert.True(res);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var obj = new RotateMatrix();
+        var res = obj.CanJump(new int[] { 0 });
+        Assert.True(res);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var obj = new RotateMatrix();
+        var res = obj.CanJump(new int[] { 1 });
+        Assert.True(res);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var obj = new RotateMatrix();
+        var res = obj.CanJump(new int[] { 0, 2, 3 });
+        Assert.False(res);
+    }
+}
+
+public class RotateMatrixTest
+{
+    [Fact]
+    public void RotateRightTest1()
+    {
+        var obj = new RotateMatrix();
+        int[,] matrix = new int[4, 3] {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+            {10,11,12}
+        };
+        var res = obj.RotateRight(matrix);
+        var expected = new int[3, 4] {
+            {10, 7, 4, 1},
+            {11, 8, 5, 2},
+            {12, 9, 6, 3},
+        };
+        Assert.Equal(expected, res);
+    }
+
+    [Fact]
+    public void RotateLeftTest1()
+    {
+        var obj = new RotateMatrix();
+        int[,] matrix = new int[4, 3] {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+            {10,11,12}
+        };
+        var res = obj.RotateLeft(matrix);
+        var expected = new int[3, 4] {
+            {3, 6, 9, 12},
+            {2, 5, 8, 11},
+            {1, 4, 7, 10},
+        };
+        Assert.Equal(expected, res);
+    }
+}
---
+++ rotate_matrix/RotateMatrix/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace MainNS
+{
+    public class RotateMatrix
+    {
+        // leetcode #55
+        public bool CanJump(int[] nums)
+        {
+            int[] dp = new int[nums.Length];
+            Array.Fill(dp, int.MaxValue);
+            dp[0] = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) break;
+                for (int j = i + 1; j <= i + nums[i] && j < nums.Length; j++)
+                {
+                    int curr = nums[i] + 1;
+                    dp[j] = Math.Min(curr, dp[j]);
+                }
+            }
+            return dp[nums.Length - 1] != int.MaxValue;
+        }
+
+        public int[,] RotateRight(int[,] matrix)
+        {
+            // https://stackoverflow.com/questions/9404683/how-to-get-the-length-of-row-column-of-multidimensional-array-in-c
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] table = new int[cols, rows];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    table[col, rows - row - 1] = matrix[row, col];
+                }
+            }
+            return table;
+        }
+
+        public int[,] RotateLeft(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] table = new int[cols, rows];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    table[cols - col - 1, row] = matrix[row, col];
+                }
+            }
+            return table;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Jump Game");
+        }
+    }
+}
